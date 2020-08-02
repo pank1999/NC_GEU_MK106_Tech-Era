090461cec502c148df4192a3d12e3b6ca057863c
pank1999 <69034032+pank1999@users.noreply.github.com>
2020-08-02 16:46:36 +0530

    Update Homepage.php
    not neccessary

--- Tech-era-career-counselling-project/hackathon-website/startbootstrap-modern-business-gh-pages/Homepage.php
+++ Tech-era-career-counselling-project/hackathon-website/startbootstrap-modern-business-gh-pages/Homepage.php
@@ -543,12 +543,7 @@ include('../include/csslinks.php');
   include('../include/footer.php');
   ?>
 
-  <!-- Footer -
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-    </div>
--->
+  
     <!-- /.container -->
   </footer>
 
