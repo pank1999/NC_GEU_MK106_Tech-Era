291820aab5e782ef50f5528742d10515de4e61d0
pushpendra tiwari <69035645+1pushpendratiwari@users.noreply.github.com>
2020-08-02 15:05:31 +0530

    disabled php validation for interest assessmetnt

--- Tech-era-career-counselling-project/hackathon-website/class_8_10_interest/Assignments_01_01.php
+++ Tech-era-career-counselling-project/hackathon-website/class_8_10_interest/Assignments_01_01.php
@@ -18,11 +18,7 @@ or die(mysqli_error($con));
 
 
 
-if( mysqli_num_rows($user_check_query )> 0){
-    $_SESSION['interest_10']=1;
-echo "<script>alert('you have already submitted')</script>";
-echo "<script>location.href = '../dashboard.php'</script>";
-}
+
 ?>
 
 <!DOCTYPE html>
