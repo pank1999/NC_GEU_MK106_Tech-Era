13d6d372c015aad016d3e9d392d10c6632a3f3ac
chitranshkhare20 <69035444+chitranshkhare20@users.noreply.github.com>
2020-08-02 20:14:07 +0530

    removed unnneccesary  validation

--- Tech-era-career-counselling-project/hackathon-website/Academic.php
+++ Tech-era-career-counselling-project/hackathon-website/Academic.php
@@ -13,12 +13,6 @@ or die(mysqli_error($con));
 
 
 
-if( mysqli_num_rows($user_check_query )> 0){
-  $_SESSION['Academic_10']=1;
-echo "<script>alert('you have already submitted')</script>";
-echo "<script>location.href = 'dashboard.php'</script>";
-
-}
 ?>
 
 
