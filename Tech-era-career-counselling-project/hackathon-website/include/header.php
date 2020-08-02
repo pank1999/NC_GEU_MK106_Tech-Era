78ea995914caf91bbcc4f4bb3db565c65bbba67c
pank1999 <69034032+pank1999@users.noreply.github.com>
2020-08-02 14:57:17 +0530

    Update header.php
    some important  changes

--- Tech-era-career-counselling-project/hackathon-website/include/header.php
+++ Tech-era-career-counselling-project/hackathon-website/include/header.php
@@ -3,15 +3,15 @@
   <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
   <a href="http://localhost/phplessons/hackathon-website/startbootstrap-modern-business-gh-pages/Homepage.php" style="text-decoration:none;"class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>Home</a>
   <a href="http://localhost/phplessons/hackathon-website/startbootstrap-modern-business-gh-pages/index.php" style="text-decoration:none;"class="w3-bar-item w3-button w3-hide-small w3-hover-white ">Career</a>
-  <a href="#work" class="w3-bar-item w3-button w3-hide-small w3-hover-white"style="text-decoration:none;">Streams</a>
+  <a href="http://localhost/phplessons/hackathon-website/expert.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white"style="text-decoration:none;">Advice</a>
 
   <a href="#contact" class="w3-bar-item w3-button w3-hide-small w3-hover-white"style="text-decoration:none;">Contact</a>
     <div class="w3-dropdown-hover w3-hide-small">
     <button  class="w3-button" title="Notifications">Select <i class="fa fa-caret-down"></i></button>     
     <div class="w3-dropdown-content w3-card-4 w3-bar-block">
-      <a href="#" class="w3-bar-item w3-button w3-hover-teal" style="background-color:;" style="text-decoration:none;">class 8-10</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-teal" style="background-color:"style="text-decoration:none;">class 11 & 12</a>
-      <a href="#" class="w3-bar-item w3-button w3-hover-teal" style="background-color:"style="text-decoration:none;">Graduation</a>
+      <a href="http://localhost/phplessons/hackathon-website/Dashboard01.php" class="w3-bar-item w3-button w3-hover-teal" style="background-color:;" style="text-decoration:none;">class 8-10</a>
+      <a href="http://localhost/phplessons/hackathon-website/Dashboard01.php" class="w3-bar-item w3-button w3-hover-teal" style="background-color:"style="text-decoration:none;">class 11 & 12</a>
+      <a href="http://localhost/phplessons/hackathon-website/Dashboard01.php" class="w3-bar-item w3-button w3-hover-teal" style="background-color:"style="text-decoration:none;">Graduation</a>
     </div>
   </div>
   <a href="http://localhost/phplessons/hackathon-website/logout.php" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="logout"><span class="glyphicon glyphicon-log-out"></span>logout</a>
