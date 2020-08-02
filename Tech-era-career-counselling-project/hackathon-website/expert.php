24d17ebf2499d0b00e38ad130777fa41a52433b0
pank1999 <69034032+pank1999@users.noreply.github.com>
2020-08-02 14:44:41 +0530

    Add files via upload
    this is correct

---
+++ Tech-era-career-counselling-project/hackathon-website/expert.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>expert</title>
+    <?php 
+      include('include/csslinks.php');
+      include('include/header.php');
+    
+    ?>
+</head>
+<body>
+     <div class="container">
+            <div class="jumbotron "style="margin-top:50px; background-color:rgba(80, 228, 178, 0.876);">
+               <h1 style="text-align:center;">Expert Advice</h1>
+            </div>
+     </div>
+
+    <!-- Team Container -->
+ 
+<div class="w3-container w3-padding-64 w3-center" id="team">
+<h2 style="text-decoration:underline;">OUR Experts</h2>
+<p>For any query or dought contact our experts of related fields</p>
+<div class="w3-row"><br>
+<div class="w3-quarter">
+  <img src="http://localhost/phplessons/hackathon-website/image/pankajimg.jpg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Pankaj  kumar pandey</h3>
+  <p>(Computer science engineer)</p>
+  <p style="color:skyblue;">Email:pankajneeraj541@gmail.com</p>
+  <p style="color:skyblue;">phone number: 8630211986</p>
+</div>
+<div class="w3-quarter">
+  <img src="http://localhost/phplessons/hackathon-website/image/chit.jpeg" alt="Boss" style="width:45%" class="w3-circle w3-hover-opacity">
+  <h3>Chitransh khare</h3>
+  <p>Web Developer</p>
+  <p style="color:skyblue;">Email:chitranshkhere20@gmail.com</p>
+  <p style="color:skyblue;">Phone number:9826223659</p>
+</div>
+<div class="w3-quarter">
+  <img src="http://localhost/phplessons/hackathon-website/image/D.jpg" alt="Boss" style="width:45% ;height:130px; border-radius:50px;" class="w3-circle w3-hover-opacity">
+  <h3>counsellor</h3>
+  <p>Doctor</p>
+  <p style="color:skyblue;">Email:chitranshkhere20@gmail.com</p>
+  <p style="color:skyblue;">Phone number:9372******</p>
+</div>
+<div class="w3-quarter">
+  <img src="http://localhost/phplessons/hackathon-website/image/lawyer.png" alt="Boss" style="width:45% ;heigth:130px;" class="w3-circle w3-hover-opacity">
+  <h3>counsellor</h3>
+  <p>lawyer</p>
+  <p style="color:skyblue;">Email:xyz@gmail.com</p>
+  <p style="color:skyblue;">Phone number:9589******</p>
+</div>
+
+
+</div>
+</div>
+
+<div class="container">
+    <div class="thumbnail">
+        <h2 style="text-align:center;">To explore more about the fields click to buttom</h2>
+         <button class="btn btn-success " style="margin-left:500px;">
+          <a href="http://localhost/phplessons/hackathon-website/startbootstrap-modern-business-gh-pages/index.php" style="font-size:20px;">Know more</a>
+          </button>
+    </div>
+
+</div>
+<?php  include('include/footer.php');?>
+
+    
+</body>
+</html>
